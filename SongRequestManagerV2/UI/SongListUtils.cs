d1d27d9e7311307644df5d8e0ba3dbf7a8af976d
Luciano Spalla <spallaluciano@outlook.com>
2025-06-25 17:33:11 -0300

    Revert song list

--- SongRequestManagerV2/UI/SongListUtils.cs
+++ SongRequestManagerV2/UI/SongListUtils.cs
@@ -4,6 +4,7 @@ using SongCore;
 using SongRequestManagerV2.Configuration;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
@@ -30,28 +31,18 @@ namespace SongRequestManagerV2.UI
             // get the tab bar
             var segcontrol = this._selectLevelCategoryViewController.GetField<IconSegmentedControl, SelectLevelCategoryViewController>("_levelFilterCategoryIconSegmentedControl");
             segcontrol.SelectCellWithNumber(index);
-            var segControl = this._selectLevelCategoryViewController
-                .GetField<IconSegmentedControl, SelectLevelCategoryViewController>("_levelFilterCategoryIconSegmentedControl");
-            segControl.SelectCellWithNumber(index);
-
-            var selectCategoryMethod = typeof(SelectLevelCategoryViewController)
-                .GetMethod("HandleLevelFilterCategoryIconSegmentedControlDidSelectCell",
-                BindingFlags.NonPublic | BindingFlags.Instance);
-            _ = selectCategoryMethod?.Invoke(this._selectLevelCategoryViewController, new object[] { index });
+            var method = typeof(SelectLevelCategoryViewController).GetMethod("SelectLevelCategoryViewController", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            _ = (method?.Invoke(this._selectLevelCategoryViewController, new object[] { segcontrol, index }));
         }
 
         public IEnumerator ScrollToLevel(string levelID, Action callback, bool isWip = false)
         {
             if (this._levelCollectionViewController) {
                 // Make sure our custom songpack is selected
-                // Index 2 corresponds to the "Custom Levels" tab in newer game versions
-                // Using index 1 would select the "Favorites" tab and trigger an
-                // ArgumentOutOfRangeException in UpdateCustomSongs
-                this.SelectCustomSongPack(2);
+                this.SelectCustomSongPack(1);
 
-                var updateCustomSongsMethod = typeof(LevelFilteringNavigationController)
-                    .GetMethod("UpdateCustomSongs", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-                _ = updateCustomSongsMethod?.Invoke(this._levelFilteringNavigationController, new object[0]);
+                var method = typeof(LevelFilteringNavigationController).GetMethod("UpdateCustomSongs", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+                _ = method.Invoke(this._levelFilteringNavigationController, new object[0]);
 
                 yield return new WaitWhile(() => this._levelFilteringNavigationController.GetField<CancellationTokenSource, LevelFilteringNavigationController>("_cancellationTokenSource") != null);
                 var gridView = this._annotatedBeatmapLevelCollectionsViewController.GetField<AnnotatedBeatmapLevelCollectionsGridView, AnnotatedBeatmapLevelCollectionsViewController>("_annotatedBeatmapLevelCollectionsGridView");
@@ -59,9 +50,8 @@ namespace SongRequestManagerV2.UI
                 var customSong = isWip
                     ? gridView._annotatedBeatmapLevelCollections.ElementAt(1)
                     : gridView._annotatedBeatmapLevelCollections.FirstOrDefault();
-                var selectCollectionMethod = typeof(AnnotatedBeatmapLevelCollectionsViewController)
-                    .GetMethod("HandleDidSelectAnnotatedBeatmapLevelCollection", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-                _ = selectCollectionMethod?.Invoke(this._annotatedBeatmapLevelCollectionsViewController, new object[] { customSong });
+                method = typeof(AnnotatedBeatmapLevelCollectionsViewController).GetMethod("HandleDidSelectAnnotatedBeatmapLevelCollection", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+                _ = (method?.Invoke(this._annotatedBeatmapLevelCollectionsViewController, new object[] { customSong }));
                 var song = isWip ? Loader.GetLevelById($"custom_level_{levelID.Split('_').Last().ToUpper()} WIP") : Loader.GetLevelByHash(levelID.Split('_').Last());
                 if (song == null) {
                     yield break;
@@ -77,27 +67,12 @@ namespace SongRequestManagerV2.UI
                 // get the table view
                 var levelsTableView = this._levelCollectionViewController.GetField<LevelCollectionTableView, LevelCollectionViewController>("_levelCollectionTableView");
                 levelsTableView.SelectLevel(song);
-
-                var detailViewController = this._levelCollectionViewController
-                    .GetField<StandardLevelDetailViewController, LevelCollectionViewController>("_levelDetailViewController");
-                if (detailViewController != null) {
-                    var diffControl = detailViewController.GetField<object, StandardLevelDetailViewController>("_beatmapDifficultySegmentedControl");
-                    if (diffControl != null && Enum.TryParse(RequestBotConfig.Instance.DefaultDifficulty, out BeatmapDifficulty diff)) {
-                        var diffControlType = diffControl.GetType();
-                        var selectDiffMethod = diffControlType.GetMethod("SelectCellWithNumber", BindingFlags.Instance | BindingFlags.Public);
-                        selectDiffMethod ??= diffControlType.GetMethod("SelectCellWithNumberWithoutNotify", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-                        selectDiffMethod?.Invoke(diffControl, new object[] { (int)diff });
-                    }
-                }
             }
             if (RequestBotConfig.Instance?.ClearNoFail == true) {
-                var gameplayModifiersPanelController = this._gameplaySetupViewController
-                    .GetField<GameplayModifiersPanelController, GameplaySetupViewController>("_gameplayModifiersPanelController");
-
+                var gameplayModifiersPanelController = this._gameplaySetupViewController.GetField<GameplayModifiersPanelController, GameplaySetupViewController>("_gameplayModifiersPanelController");
                 gameplayModifiersPanelController.gameplayModifiers.SetField("_noFailOn0Energy", false);
-                var refreshPanelMethod = typeof(GameplayModifiersPanelController)
-                    .GetMethod("RefreshActivePanel", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-                _ = refreshPanelMethod?.Invoke(gameplayModifiersPanelController, new object[0]);
+                var method = typeof(GameplayModifiersPanelController).GetMethod("RefreshActivePanel", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+                _ = (method?.Invoke(gameplayModifiersPanelController, new object[0]));
             }
             callback?.Invoke();
         }
